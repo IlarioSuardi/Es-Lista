39d053607bcde79ca2805e65ef54f281a5423585
SUARDI.22146 <suardi.ilario.studente@itispaleocapa.it>
2026-01-22 09:00:35 +0100

    Class Lista

--- Lista.java
+++ Lista.java
@@ -27,7 +27,6 @@ public class Lista {
 
     public void accoda(String value) {
         Nodo nuovoNodo = new Nodo(value);
-
         if (head == null) {
             head = nuovoNodo;
         } else {
@@ -44,7 +43,6 @@ public class Lista {
         if (cursor == null) {
             return null;
         }
-
         String valore = cursor.getValue();
         cursor = cursor.getNext();
         return valore;
@@ -54,7 +52,6 @@ public class Lista {
         if (indice < 0 || indice >= size) {
             return null;
         }
-
         Nodo t = head;
         for (int i = 0; i < indice; i++) {
             t = t.getNext();
@@ -62,14 +59,58 @@ public class Lista {
         return t.getValue();
     }
 
-    public String toString() {
-        Nodo t = head;
-        String risultato = "";
-
-        while (t != null) {
-            risultato += t.getValue() + " ";
-            t = t.getNext();
+    public void inserisci(String value, int indice) {
+        if (indice < 0 || indice > size) {
+            return;
         }
-        return risultato;
+        Nodo nuovoNodo = new Nodo(value);
+        if (indice == 0) {
+            nuovoNodo.setNext(head);
+            head = nuovoNodo;
+        } else {
+            Nodo t = head;
+            for (int i = 0; i < indice - 1; i++) {
+                t = t.getNext();
+            }
+            nuovoNodo.setNext(t.getNext());
+            t.setNext(nuovoNodo);
+        }
+        size++;
+    }
+
+    public void elimina(int indice) {
+        if (indice < 0 || indice >= size) {
+            return;
+        }
+        if (indice == 0) {
+            head = head.getNext();
+        } else {
+            Nodo t = head;
+            for (int i = 0; i < indice - 1; i++) {
+                t = t.getNext();
+            }
+            t.setNext(t.getNext().getNext());
+        }
+        size--;
+    }
+
+    public void eliminazioneCompleta(String value) {
+        if (head == null) {
+            return;
+        }
+        Nodo t = head;
+        while (t.getNext() != null) {
+            if (t.getNext().getValue().equals(value)) {
+                t.setNext(t.getNext().getNext());
+                size--;
+            } else {
+                t = t.getNext();
+            }
+        }
+    }
+
+    public String toString() {
+        return "[size = " + size + ", head = " + head + "cursor = " + cursor + "]";
     }
 }
+
