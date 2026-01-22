d05c2abe61f80c30e07e531e9ca9aeaed33de550
SUARDI.22146 <suardi.ilario.studente@itispaleocapa.it>
2026-01-22 09:00:27 +0100

    Class Nodo

--- Nodo.java
+++ Nodo.java
@@ -22,4 +22,8 @@ public class Nodo {
     public void setNext(Nodo next) {
         this.next = next;
     }
+
+    public String toString() {
+        return "Nodo[value = " + value + ", next = " + next + "]";
+    }
 }
